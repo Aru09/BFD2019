ed55ec9d3a683843b00b3d1c348b8b9498333bba
Aru09 <42914363+Aru09@users.noreply.github.com>
2019-09-25 13:57:57 +0600

    Add files via upload

---
+++ Project1/article/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Article, Author
+#adminka
+#qwerty123
+
+admin.site.register(Article)
+admin.site.register(Author)
+# Register your models here.
